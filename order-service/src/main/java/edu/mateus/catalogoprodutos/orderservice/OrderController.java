fd8d07a6a7050c8e338a14bd6a5ac8f6e824009b
Mateus Lima <mateuslimasantos909@gmail.com>
2025-09-29 17:55:08 +0000

    feat: Adiciona .gitignore e estrutura dos 3 serviços

--- order-service/src/main/java/edu/mateus/catalogoprodutos/orderservice/OrderController.java
+++ order-service/src/main/java/edu/mateus/catalogoprodutos/orderservice/OrderController.java
@@ -3,7 +3,6 @@ package edu.mateus.catalogoprodutos.orderservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.beans.ConstructorProperties;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- product-service/src/main/java/edu/mateus/catalogoprodutos/productservice/ProductService.java
+++ product-service/src/main/java/edu/mateus/catalogoprodutos/productservice/ProductService.java
@@ -1,6 +1,5 @@
 package edu.mateus.catalogoprodutos.productservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
